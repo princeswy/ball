6a33e52682bba1ca7ff1637c7178cd72751da770
宋文宇 <wenyu.song@zhiguoguo.com>
2020-10-27 15:01:29 +0800

    比赛列表接口修改

--- app/Http/Controllers/FmatchController.php
+++ app/Http/Controllers/FmatchController.php
@@ -17,8 +17,37 @@ class FmatchController extends Controller
 
 //
     public function show(Request $request) {
-        $match_time = $request->input('match_time') || '';
+        $date = date('Y-m-d');
+        $match_time = $request->input('match_time') ? $request->input('match_time') : $date;
+        $dateMap = [
+            date("Y-m-d",strtotime("-1 day"))
+        ];
+        for ($i = 0; $i < 4; $i++) {
+            $dateMap[] = date("Y-m-d",strtotime("+".$i." day"));
+        }
+        $res = [
+            'code' => 1,
+            'success' => true,
+            'dateList' => $dateMap,
+            'curDate' => $match_time,
+            'matchState' => [
+                0 => '未开赛',
+                1 => '上半场',
+                2 => '中场',
+                3 => '下半场',
+                4 => '加时',
+                5 => '点球',
+                '-1' => '完场',
+                '-10' => '取消',
+                '-11' => '待定',
+                '-12' => '腰斩',
+                '-13' => '中断',
+                '-14' => '推迟',
+            ]
+        ];
         $match_map = Fmatch::where('match_time', 'like', $match_time.'%')->orderBy('match_time', 'asc')->select('match_id','league_name','match_time','home_name','guest_name','match_state','half_score','score','home_red','guest_red','home_yellow','guest_yellow','home_corner','guest_corner')->get()->toarray();
-        return json_encode($match_map);
+        $res['list'] = $match_map;
+//        return json_encode($match_map);
+        return $res;
     }
 }

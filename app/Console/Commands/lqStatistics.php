53493491e6519a5cabd5cb458ff7ebdf97c4d251
宋文宇 <wenyu.song@zhiguoguo.com>
2020-11-10 16:24:22 +0800

    修改

--- app/Console/Commands/lqStatistics.php
+++ app/Console/Commands/lqStatistics.php
@@ -295,8 +295,8 @@ class lqStatistics extends  Command
 
     public static function check_process_num($script_name) {
         $cmd = @popen("ps -ef | grep '{$script_name}' | grep -v grep | wc -l", 'r');
-        $num = @fread($cmd, 512);
-        $num += 0;
+        $num = (int) @fread($cmd, 512);
+        (int) $num += 0;
         @pclose($cmd);
         if ($num > 1) {
             return false;
